060b8c136eb5f3193394355e52cd3ead69769a2d
Yannick Mau <ymdev@xmau.de>
2022-06-24 11:55:06 +0200

    add floor1 of nbs building

---
+++ assets/tilesets/gather-signage.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.5" name="gather-signage" tilewidth="32" tileheight="32" tilecount="3160" columns="40">
+ <image source="../gather_signage_1.2.png" width="1280" height="2529"/>
+</tileset>
---
+++ assets/tilesets/pkmn-indoor.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.5" name="pkmn-indoor" tilewidth="32" tileheight="32" tilecount="4792" columns="8">
+ <image source="../tileset-pkm-interior.png" width="256" height="19168"/>
+</tileset>
